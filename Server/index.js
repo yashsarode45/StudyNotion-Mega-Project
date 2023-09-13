3afe378228a3085ab75dad1392d24b0c942bf10b
Yash Sarode <yashsarode45@gmail.com>
2023-09-14 00:12:39 +0530

    CORS UPDATE FROM REF

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,12 @@ database.connectDB();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+)
 // app.use((req, res, next) => {
 // 	res.header('Access-Control-Allow-Origin', '*');
 // 	next();
