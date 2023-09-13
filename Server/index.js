480e9c1ce2fde0043f8492f551236ba80c6287d6
Yash Sarode <yashsarode45@gmail.com>
2023-09-13 22:52:35 +0530

    CORS changes

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-mega-project-frontend.vercel.app/",
 		credentials:true,
 	})
 )
